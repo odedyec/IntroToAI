16f0749043db1620cad7626cef1bfc32d91a8cb7
Oded Yechiel <odedyech@post.bgu.ac.il>
2018-11-10 21:16:02 +0200

    Updated the config.py file

--- assignment1/config.py
+++ assignment1/config.py
@@ -1,8 +1,11 @@
 from human_agent import Human
 from greedy_agent import Greedy
-# from vandal_agent import Vandal
+from vandal_agent import Vandal
+from smart_greedy_agent import SmartGreedy
 
 
 """ Global variables """
 K = None  # Set to a value to stop asking for input
 agents = []  # Set an agent to stop asking for input
+DEBUG = True
+f_value = -10
