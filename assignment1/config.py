2262e328a79abd4c2765d81686a0cbb292e663c5
matanr <matanr96@gmail.com>
2018-11-10 23:50:51 +0200

    Added a_star, not final. need to investigate whether the cost in the heuristic calculation is needed here.

--- assignment1/config.py
+++ assignment1/config.py
@@ -2,10 +2,11 @@ from human_agent import Human
 from greedy_agent import Greedy
 from vandal_agent import Vandal
 from smart_greedy_agent import SmartGreedy
+from a_star_agent import A_Star
 
 
 """ Global variables """
-K = None  # Set to a value to stop asking for input
-agents = []  # Set an agent to stop asking for input
+K = 2  # Set to a value to stop asking for input
+agents = [A_Star(0)]  # Set an agent to stop asking for input
 DEBUG = True
 f_value = -10
