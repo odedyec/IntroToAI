a2fd3bad162cc031be8d20732d9e70c3d356bc45
Oded Yechiel <odedyech@post.bgu.ac.il>
2018-11-15 09:35:59 +0200

    Updated config for defense

--- assignment1/config.py
+++ assignment1/config.py
@@ -8,7 +8,7 @@ from real_time_a_star_agent import Real_Time_A_Star
 
 """ Global variables """
 config_file='input_graph2.txt'
-K = 0.1  # Set to a value to stop asking for input
-agents = [SmartGreedy(3)]  # Set an agent to stop asking for input
+K = None  # Set to a value to stop asking for input
+agents = []  # Set an agent to stop asking for input
 DEBUG = True
 f_value = -10
